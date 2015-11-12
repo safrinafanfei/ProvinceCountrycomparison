31fb79ecd853aa3a5d0b532d6c81182ebe002488
Fan Fei <feif1021@gmail.com>
2015-11-11 21:14:49 -0800

    add 2 iframes and test fadeout
    fade out too fast.

--- package.js
+++ package.js
@@ -12,3 +12,24 @@ jQuery(function($) {
 });
 //* end of the opening chapter
 
+/* starting map1 
+<!--- $(window).scroll(function() {
+    var pxFromBottom = 400;
+    if ($(window).scrollTop() + $(window).height() > $(document).height() - pxFromBottom) {
+        $('.gdpcap').fadeOut('slow');
+    } else {
+        $('.gdpcap').fadeIn('slow')
+    }
+}); 
+
+starting map2 
+$(window).scroll(function() {
+    var pxFromBottom = 1400;
+    if ($(window).scrollTop() + $(window).height() > $(document).height() - pxFromBottom) {
+        $('.unemployment').fadeOut('slow');
+    } else {
+        $('.unemployment').fadeIn('slow')
+    }
+});    
+
+*/
